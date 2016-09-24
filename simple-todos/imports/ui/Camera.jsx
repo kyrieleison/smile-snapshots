dda9895d967c8a69a2ffb3c4c25fb45077c5d530
Yuka Sato <yuka.sato@gaiax.com>
2016-09-24 19:51:36 +0900

    wip

--- simple-todos/imports/ui/Camera.jsx
+++ simple-todos/imports/ui/Camera.jsx
@@ -3,10 +3,27 @@ import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
 class Camera extends Component {
+  componentDidMount() {
+    let localVideo = ReactDOM.findDOMNode(this.refs.local);
+    let localStream;
+
+    window.navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then((stream) => {
+      console.log(stream);
+      localStream = stream;
+      localVideo.src = window.URL.createObjectURL(localStream);
+    })
+    .catch((error)  => {
+      console.error('mediaDevice.getUserMedia() error:', error);
+      return;
+    });
+  }
   render() {
     return (
       <div className="camera">
         it's camera.
+        <br />
+        <video id="local_video" ref="local" style={{ width: 320 + 'px', height: 240 + 'px' }} />
       </div>
     );
   }
