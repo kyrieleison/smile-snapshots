c5f690c772b49343e01acba94fc5404d76d09b22
shinya <shinya@xi.mods.jp>
2016-09-25 16:59:56 +0900

    パス修正版です。

--- client-web-app/server/main.js
+++ client-web-app/server/main.js
@@ -7,6 +7,13 @@ import fs from 'fs';
 import { Photos } from '../imports/api/photos.js';
 
 Meteor.startup(() => {
+
+  // console.log(process.argv[1], "a.js", "cwd", process.cwd());
+  let paths = process.cwd().split('.');
+  console.log(paths[0]);
+  fs.writeFileSync(paths[0]+"/public/img/test.txt","testes");
+  // fs.writeFile(__dirname+'/test.txt',"aaaaaa");
+
   WebApp.connectHandlers.use("/image", (req, res, next) => {
     let data = '';
     let filename = dateformat(new Date(), 'yyyymmddHHMMss');
@@ -22,7 +29,9 @@ Meteor.startup(() => {
         console.log("Request Body:" + data);
         let image = querystring.parse(data).image;
         res.end(data);
-        fs.writeFile(`../web.browser/app/img/uploaded_${filename}.jpg`, image, 'base64', (err) => {
+
+        //fs.writeFile(`../web.browser/app/img/uploaded_${filename}.jpg`, image, 'base64', (err) => {
+        fs.writeFile(paths[0]+`/public/img/uploaded_${filename}.jpg`, image, 'base64', (err) => {
           if (err) { console.log("Error:" + err) };
           console.log('It\'s Saved');
 
