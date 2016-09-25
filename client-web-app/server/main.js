6a5efc5402c3b1c24573686f4ba8ef4facb660f8
Yuka Sato <yuka.sato@gaiax.com>
2016-09-25 11:46:20 +0900

    アップロードのレスポンスを json で返すようにした

--- client-web-app/server/main.js
+++ client-web-app/server/main.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import querystring from 'querystring';
 
 Meteor.startup(() => {
-  WebApp.connectHandlers.use("/hello", (req, res, next) => {
+  WebApp.connectHandlers.use("/image", (req, res, next) => {
     // console.log(Object.keys(req));
     // リクエストデータ処理
     let data = '';
@@ -10,18 +10,17 @@ Meteor.startup(() => {
       console.log("Receive POST");
       //readableイベントが発火したらデータにリクエストボディのデータを追加
       req.on('readable', (chunk) => {
-            data += req.read();
-        });
+        data += req.read();
+      });
       //リクエストボディをすべて読み込んだらendイベントが発火する。
       req.on('end', () => {
         //パースする
         querystring.parse(data);
-          res.end(data);
-          console.log("Request Body:"+data);
+        res.end(data);
       });
     }
-    res.writeHead(200);
-    res.end("Hello world from: " + Meteor.release);
-  })
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ payload: 'ok' }));
+  });
 });
 
