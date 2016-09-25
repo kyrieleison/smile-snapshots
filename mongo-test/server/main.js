07d981b5d4fc9e0254e730d8e20537557c0de19a
shinya <shinya@xi.mods.jp>
2016-09-25 12:36:07 +0900

    ファイル更新をフロントへ通知します。

--- mongo-test/server/main.js
+++ mongo-test/server/main.js
@@ -1,14 +1,16 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
 import querystring from 'querystring';
 import base64 from 'urlsafe-base64';
 import fs from 'fs';
+import {Photos } from '../imports/api/photos.js'
 
 Meteor.startup(() => {
   WebApp.connectHandlers.use("/hello", (req, res, next) => {
     // console.log(Object.keys(req));
     // リクエストデータ処理
     let data = '';
-    if (req.method === 'POST') { 
+    /* if (req.method === 'POST') { 
       console.log("Receive POST");
       //readableイベントが発火したらデータにリクエストボディのデータを追加
       req.on('readable', (chunk)　=> {
@@ -20,14 +22,33 @@ Meteor.startup(() => {
         querystring.parse(data);
           res.end(data);
           console.log("Request Body:"+data);
+          
           let img =  base64.decode( data );
           fs.writeFile('../web.browser/app/img/uploaded.jpg',img,(err) =>{
             console.log("error:"+(err));  
           }) 
-      });
-    }
+      }); 
+    } */
+    // 受信したファイル名を記録
+
+    // Todos = new Mongo.Collection('Todos');
+    // Todos.insert({_id: 'my-todo'});
+
+    Photos.insert({
+      userID: 1,
+      user: '利用者A',
+      url:'img/images.jpg',
+      shopID: 1,
+      shopName:'A店',
+      createdAt: new Date()
+    }); 
+
+    /* Meteor.publish("update_images", () => {
+      console.log("notify upload");
+      this.ready();
+    }); */
+
     res.writeHead(200);
     res.end("Hello world from: " + Meteor.release);
   })
 });
-
