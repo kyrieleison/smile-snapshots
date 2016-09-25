101a96a1f1c1f5a9907a61f70812d3ee9736722f
Yuka Sato <yuka.sato@gaiax.com>
2016-09-25 15:10:42 +0900

    wip

--- client-web-app/server/main.js
+++ client-web-app/server/main.js
@@ -1,28 +1,43 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
 import querystring from 'querystring';
+import dateformat from 'dateformat';
 import fs from 'fs';
+
+import { Photos } from '../imports/api/photos.js'
 
 Meteor.startup(() => {
   WebApp.connectHandlers.use("/image", (req, res, next) => {
-    // console.log(Object.keys(req));
-    // リクエストデータ処理
     let data = '';
+    let filename = dateformat(new Date(), 'yyyymmddHHMMss');
+
     if (req.method === 'POST') { 
       console.log("Receive POST");
-      //readableイベントが発火したらデータにリクエストボディのデータを追加
+
       req.on('readable', (chunk) => {
         data += req.read();
       });
-      //リクエストボディをすべて読み込んだらendイベントが発火する。
+
       req.on('end', () => {
         console.log("Request Body:" + data);
         let image = querystring.parse(data).image;
         res.end(data);
-        fs.writeFile('../web.browser/app/img/uploaded.jpg', image, 'base64', (err) => {
-          console.log("error:" + (err));
+        fs.writeFile(`../web.browser/app/img/uploaded/${filename}.jpg`, image, 'base64', (err) => {
+          if (err) { console.log("Error:" + err) };
+          console.log('It\'s Saved');
+
+          Photos.insert({
+            userID: 1,
+            user: '山田太郎',
+            url: `img/uploaded/${filename}.jpg`,
+            shopID: 1,
+            shopName:'A店',
+            createdAt: new Date()
+          });
         })
       });
     }
+
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ payload: 'ok' }));
   });
