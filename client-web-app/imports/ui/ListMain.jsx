506560f83c243f40b68692e5b624889e1ce53377
Yuka Sato <yuka.sato@gaiax.com>
2016-09-25 15:42:22 +0900

    画面が表示されるようにした

--- client-web-app/imports/ui/ListMain.jsx
+++ client-web-app/imports/ui/ListMain.jsx
@@ -1,20 +1,18 @@
 import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
 import ListImages from './ListImages';
 
-const imgs = [
-      { id:1, url:'img/images.jpg',msg:'A店' },
-      { id:2, url:'img/images.jpg',msg:'B店' },
-      { id:3, url:'img/images.jpg',msg:'C店' },
-      { id:4, url:'img/images.jpg',msg:'観光地A' },
-      { id:5, url:'img/images.jpg',msg:'観光地B' },
-    ];
-    
-// Task component - represents a single todo item
-export default class Home extends Component {
-  
-  /* TODO: iPhoneから送信されてきた画像をnode.jsでpublic/imgに保存した後、
-  　　画像一覧のURLをimgsを参考に生成してListImagesに渡してください。　*/
+import { Photos } from '../api/photos.js'
 
+// const imgs = [
+//       { id:1, url:'img/images.jpg',msg:'A店' },
+//       { id:2, url:'img/images.jpg',msg:'B店' },
+//       { id:3, url:'img/images.jpg',msg:'C店' },
+//       { id:4, url:'img/images.jpg',msg:'観光地A' },
+//       { id:5, url:'img/images.jpg',msg:'観光地B' },
+//     ];
+    
+export default class ListMain extends Component {
   constructor(props) {
     super(props);
   }
@@ -22,10 +20,18 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <ListImages images={imgs} />
+        <ListImages images={this.props.imgsMongo} />
       </div>
     );
   }
 } 
- 
 
+export default createContainer(() => {
+ // Meteor.subscribe("server_states");
+ // console.log("recive upload");
+ // this.imgsMongo = Photos.find({});
+ // console.log("MongoDB:"+Photos.find({}));
+  return {
+    imgsMongo : Photos.find({}).fetch(),
+  };
+}, ListMain);
